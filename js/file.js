59111d21a04d755c3c51c3694b0b5d2fc0960d70
Cyanide1013 <87561494+Cyanide1013@users.noreply.github.com>
2022-09-01 15:19:42 +0530

    Update file.js

---
+++ js/file.js
@@ -0,0 +1,5 @@
+var name= prompt("enter ur name:");
+var firstLetter= name.slice(0,1);
+var remaingLetter= name.slice(1,name.length);
+var firstLetterUpper=firstLetter.toUpperCase();
+alert("hello "+ firstLetterUpper + remaingLetter);
